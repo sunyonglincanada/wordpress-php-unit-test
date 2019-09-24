38ad37d6b04543a3d71adc060b749d64adf6cad8
Eric Sun <eric.sun@trader.ca>
2019-09-24 15:07:19 -0400

    MOD: add comment for sample test case

--- tests/phpunit/tests/sample.php
+++ tests/phpunit/tests/sample.php
@@ -1,6 +1,18 @@
 <?php
+/**
+ * Sample Test Case
+ *
+ * Notes:
+ * cat_is_ancestor_of is validated under test\term\term_is_ancestor_of
+ *
+ * @group sample.php
+ */
+
 class SampleTest extends WP_UnitTestCase {
 
+    /**
+     * Add trailing slash when none is present
+     */
     public function test_trailingslashit_should_add_slash_when_none_is_present() {
         $this->assertSame( 'foo/', trailingslashit( 'foo' ) );
     }
